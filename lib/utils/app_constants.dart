9871d9b647a6500a94cfdcfbae72eee2b937800a
Shakil Ahmed <shakilahmed.of@gmail.com>
2025-11-19 18:53:08 +0600

    sss

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,8 +1,6 @@
 class ApiConstants {
-  static const baseUrl =
-      "https://corsproxy.io/?https://www.pqstec.com/InvoiceApps/Values/";
-  static const imageBase =
-      "https://www.pqstec.com/InvoiceApps/";
+  static const baseUrl = "https://www.pqstec.com/InvoiceApps/Values/";
+  static const imageBase = "https://www.pqstec.com/InvoiceApps/";
   static const loginEndpoint = "LogIn";
   static const customerListEndpoint = "GetCustomerList";
   static const defaultComId = 1;
